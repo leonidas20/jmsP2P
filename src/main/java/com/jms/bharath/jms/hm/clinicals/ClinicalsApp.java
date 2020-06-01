6ea56c147f66ac95e404df242fa0ba1ebdd1af1c
LT <ltheodor@hotmail.de>
2020-06-01 20:29:09 +0200

    44. Create the clinical app and send a message

---
+++ src/main/java/com/jms/bharath/jms/hm/clinicals/ClinicalsApp.java
@@ -0,0 +1,43 @@
+package com.jms.bharath.jms.hm.clinicals;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import com.jms.bharath.jms.hm.model.Patient;
+
+public class ClinicalsApp {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+
+		InitialContext initialContext = new InitialContext();
+		Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+		
+		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext();) {
+			
+			JMSProducer producer = jmsContext.createProducer();
+			
+			ObjectMessage objectMessage = jmsContext.createObjectMessage(); 
+			Patient patient = new Patient();
+			patient.setId(123);
+			patient.setName("Bob");
+			patient.setInsuranceProvider("Blue Cross Blue Shield");
+			patient.setCopay(30d);
+			patient.setAmoutToBePayed(500d);
+			
+			objectMessage.setObject(patient);
+			
+			producer.send(requestQueue, objectMessage);
+			
+		}
+		
+	}
+
+}
